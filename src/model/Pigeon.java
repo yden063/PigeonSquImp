6142e12155f344f4fe5773bc6a75d609e00db82e
Youness Dendane <youness.dendane1@uqac.ca>
2018-10-24 21:30:45 -0400

    First version of Pigeon

--- src/model/Pigeon.java
+++ src/model/Pigeon.java
@@ -4,6 +4,7 @@ import java.awt.Point;
 import java.util.List;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 
 public class Pigeon implements Runnable {
 
@@ -13,7 +14,6 @@ public class Pigeon implements Runnable {
 	private Object square;
 
 	public Pigeon(int x, int y, Color color) {
-		this.color = color;
 		this.point = new Point(x, y);
 		this.isAlive = true;
 	}
@@ -26,14 +26,46 @@ public class Pigeon implements Runnable {
 	}
 
 	private void live() {
+		goToFood();
+	}
+
+	private void goToFood() {
 		List<Food> foods = square.getFoods();	
-		findFood();
-	}
-
-	private void findFood() {
-		// TODO Auto-generated method stub
+		Point p = findFood(foods);
 		
+		if (p != null) {
+			moveTo(p);
+		}
+	}
+	
+	private Point findFood(List<Food> foods) {
+		Point p = null;
+		
+		if (foods.size() > 0) {
+			Double distance = calculateDistance(foods.get(0));
+			p = foods.get(0);
+			
+			for (int i = 1; i < foods.size(); i++) {	
+				if (calculateDistance(foods.get(i)) < distance) {
+					p = foods.get(i);
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	private Double calculateDistance(Point p1) {
+		Double sumX = Math.pow(p1.x - point.x, 2);
+		Double sumY = Math.pow(p1.y - point.y, 2);
+		
+		return Math.sqrt(sumX + sumY);
 	}
 
-
+	private void moveTo(Point p) {
+		if (point.x<p.x) point.x++; 
+		if (point.x>p.x) point.x--; 
+		if (point.y<p.y) point.y++; 
+		if (point.y>p.y) point.y--; 
+	}
 }
